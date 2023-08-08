ab418492d12bafca5f5f7899353f86aca9b2eb3f
dBodi <51955398+robespierre81@users.noreply.github.com>
2023-08-08 13:15:47 +0600

    Make the Investment Flowbox working with React

---
+++ dashboard/subprofile_detail.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Profile - Amura Holding LLC</title>
+    <meta charset="utf-8">
+    <meta name="keywords" content="Amura AI3 Holding LLC">
+    <meta name="description" content="Amura AI3 Holding LLC">
+    <meta name="generator" content="Amura AI3 Holding LLC">
+    <meta property="og:title" content="Amura AI3 Holding LLC">
+    <meta property="og:description" content="Amura AI3 Holding LLC">
+    <meta property="og:image" content="https://www.amura.io/images/Amura_Favicon.png">
+    <meta property="og:url" content="https://dashboard.amura.io">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/Amura_Favicon.png">
+    <link rel="stylesheet" type="text/css" href="/css/dashboard.css">
+    <link rel="stylesheet" type="text/css" href="/css/content.css">
+    <link rel="stylesheet" type="text/css" href="/css/forms.css">
+</head>
+
+<body>
+    
+    <div class="divpadd">
+        <h4>Profile</h2>
+        
+        <div class="div-parent-row-2">
+            
+            <div class="div-child-left-50">
+                <!-- Form Structure -->
+                <div style="max-width: 800px;">
+                <form id="profile-form">
+                    
+                  <div class="form__group field">  
+                    <div class="form__field-container">
+                      <input type="input" class="form__field" placeholder="IDA-Main-Wallet" required>
+                      <label class="form__label">ID.AMURA Main Wallet Address</label>
+                    </div>
+                    </div>  
+                    
+                
+                  <div class="form__group field">
+                    
+                      <div class="form__field-container">
+                      <input type="input" class="form__field" placeholder="First Name" required>
+                      <label class="form__label">First Name</label>
+                    </div>
+                  </div>
+                  <div class="form__group field">
+                    <div class="form__field-container">
+                      <input type="input" class="form__field" placeholder="Last Name" required>
+                      <label class="form__label">Last Name</label>
+                    </div>
+                  </div>
+                  <div class="form__group field">
+                    <input type="input" class="form__field" placeholder="Street" required>
+                    <label for="street" class="form__label">Street</label>
+                  </div>
+                  <div class="form__group field">
+                    <div class="form__field-container">
+                      <input type="input" class="form__field" placeholder="ZIP Code" required>
+                      <label class="form__label">ZIP Code</label>
+                    </div>
+                  </div>
+                  <div class="form__group field">
+                    <div class="form__field-container">
+                      <input type="input" class="form__field" placeholder="City" required>
+                      <label class="form__label">City</label>
+                    </div>
+                  </div>
+                  <div class="form__group field">
+                    <input type="input" class="form__field" placeholder="County" required>
+                    <label for="county" class="form__label">County</label>
+                  </div>
+                  <div class="form__group field">
+                    <input type="email" class="form__field" placeholder="Email Address" required>
+                    <label for="email" class="form__label">Email Address</label>
+                  </div>
+                  <div class="form__group field">
+                    <input type="tel" class="form__field" placeholder="Phone Number" required>
+                    <label for="phone" class="form__label">Phone Number</label>
+                  </div>
+                <br>
+                  <button type="submit" class="btn-primary">Update</button>
+                </form>
+                </div>
+            </div>
+            
+            <div class="div-child-flex-1">
+                <div class="div-parent-column">
+                
+                <div class="div-full-outline"> 
+                    <h4>KYC</h2> 
+                    <p>25/03/2023 - DONE</p>
+                    <div class="button-container">
+                        <a class="tag-icon-red-full" href="#">Update</a>
+                        <a class="tag-icon-blue-full" href="#">Show</a>
+                    </div>
+                </div>
+                    <br>
+                <div class="div-full-outline"> 
+                    <h4>Password</h2>
+                    <div class="button-container">
+                    <br>
+                        <a class="tag-icon-blue-full" href="#">change</a>
+                        <a class="tag-icon-blue-full" href="#">show</a>
+                        <a class="tag-icon-blue-full" href="#">block</a>
+                    </div>
+                </div>
+                    <br>
+                <div class="div-full-outline"> 
+                  <h4>2FA Security</h4>
+                  <div class="button-container">
+                    <a class="tag-icon-blue-full" href="#">Activate</a>
+                  </div>
+                </div>
+
+                    <br>
+                </div>
+            
+            
+            
+            </div>
+            </div>
+        
+
+
+
+        
+        
+  
+
+        
+        
+        
+        
+        
+
+<br><br><br><br><br>
+        
+
+
+    </div>  
+</body>
+</html>
